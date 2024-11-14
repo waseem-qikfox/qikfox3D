a48781668cd8e3b731abd04a429806629a3d5184
waseem-qikfox <waseem.h@qikfox.com>
2024-11-15 05:08:37 +0530

    Updated rebranding script and qikfox3D resources

--- init.js
+++ init.js
@@ -1,99 +1,126 @@
-// const { execSync } = require('child_process');
-// const fs = require('fs');
-// const path = require('path');
-// const { DOMParser, XMLSerializer } = require('xmldom');
-
-// function cloneRepository(repoUrl, cloneDir) {
-//     if (fs.existsSync(cloneDir)) {
-//         console.log(`Directory ${cloneDir} already exists. Deleting...`);
-//         fs.rmdirSync(cloneDir, { recursive: true });
-//     }
-//     execSync(`git clone ${repoUrl} ${cloneDir}`, { stdio: 'inherit' });
-// }
-
-// function replaceText(nodeValue) {
-//     return nodeValue
-//         .replace(/firestorm/gi, 'qikfox3D');
-// }
-
-// function modifyXml(filePath) {
-//     let data = fs.readFileSync(filePath, 'utf-8');
-//     data = replaceText(data); // Replace all instances in the raw string
-
-//     const doc = new DOMParser().parseFromString(data, 'application/xml');
-
-//     function traverseAndModify(node) {
-//         if (node.nodeType === 3 || node.nodeType === 8) { // Text node or Comment node
-//             node.nodeValue = replaceText(node.nodeValue);
-//         }
-//         if (node.attributes) {
-//             for (let i = 0; i < node.attributes.length; i++) {
-//                 let attr = node.attributes[i];
-//                 attr.value = replaceText(attr.value);
-//             }
-//         }
-//         for (let child = node.firstChild; child; child = child.nextSibling) {
-//             traverseAndModify(child);
-//         }
-//     }
-
-//     traverseAndModify(doc);
-
-//     const serializer = new XMLSerializer();
-//     const xmlString = serializer.serializeToString(doc);
-//     fs.writeFileSync(filePath, xmlString);
-// }
-
-// function modifyFilesInDirectory(directory) {
-//     fs.readdirSync(directory).forEach(file => {
-//         const filePath = path.join(directory, file);
-//         if (fs.statSync(filePath).isDirectory()) {
-//             modifyFilesInDirectory(filePath);
-//         } else if (filePath.endsWith('.xml') || filePath.endsWith('.xib')) {
-//             modifyXml(filePath);
-//         }
-//     });
-// }
-
-// function renameScript(directory) {
-//     const oldPath = path.join(directory, 'scripts', 'configure_firestorm.sh');
-//     const newPath = path.join(directory, 'scripts', 'configure_qikfox3D.sh');
-
-//     if (fs.existsSync(oldPath)) {
-//         fs.renameSync(oldPath, newPath);
-//         console.log(`Renamed ${oldPath} to ${newPath}`);
-//     } else {
-//         console.log(`File ${oldPath} does not exist.`);
-//     }
-// }
-
-// function main() {
-//     const repoUrl = 'https://github.com/qikfox3DViewer/phoenix-firestorm.git';
-//     const cloneDir = 'qikfox3D-viewer';
-//     cloneRepository(repoUrl, cloneDir);
-//     modifyFilesInDirectory(cloneDir);
-//     renameScript(cloneDir);
-// }
-
-// main();
-
-
 const { execSync } = require('child_process');
 const fs = require('fs');
 const path = require('path');
 
-function cloneRepository(repoUrl, cloneDir) {
+function cloneRepository(repoUrl, cloneDir, commitHash) {
     if (fs.existsSync(cloneDir)) {
-        console.log(`Directory ${cloneDir} already exists. Deleting...`);
-        fs.rmdirSync(cloneDir, { recursive: true });
+        console.log(`Directory ${cloneDir} already exists. Exiting...`);
+        return false;
     }
     execSync(`git clone ${repoUrl} ${cloneDir}`, { stdio: 'inherit' });
+    execSync(`git -C ${cloneDir} checkout ${commitHash}`, { stdio: 'inherit' });
+    return true;
+}
+
+function replaceInFile(filePath, searchValue, replaceValue) {
+    const content = fs.readFileSync(filePath, 'utf8');
+    
+    if (content.includes(searchValue)) {
+        const newContent = content.replace(new RegExp(searchValue, 'g'), replaceValue);
+        fs.writeFileSync(filePath, newContent, 'utf8');
+    }
+}
+
+function traverseAndReplace(dir, searchValue, replaceValue) {
+    const files = fs.readdirSync(dir);
+    files.forEach(file => {
+        const fullPath = path.join(dir, file);
+
+        if (fs.statSync(fullPath).isDirectory()) {
+            if (!file.startsWith('.git')) {
+                traverseAndReplace(fullPath, searchValue, replaceValue);
+            }
+        } else if (
+            !file.endsWith('.jpeg') && 
+            !file.endsWith('.jpg') && 
+            !file.endsWith('.png') && 
+            !file.endsWith('.icns') && 
+            !file.endsWith('.ico') &&
+            !file.endsWith('.r') &&
+            !file.startsWith('.git')
+        ) {
+            replaceInFile(fullPath, 'Firestorm', 'qikfox3D');
+
+            if (file.endsWith('.cpp') || file.endsWith('.xml')) {
+                replaceInFile(fullPath, 'http://phoenixviewer.com/app/loginV3/', 'https://qikfox.com/3d-browser/');
+                replaceInFile(fullPath, 'https://phoenixviewer.com/app/loginV3/', 'https://qikfox.com/3d-browser/');
+            }
+        }
+    });
+}
+
+function renamePlistFile(directory) {
+    const plistOldPath = path.join(directory, 'indra', 'newview', 'Info-Firestorm.plist');
+    const plistNewPath = path.join(directory, 'indra', 'newview', 'Info-qikfox3D.plist');
+    if (fs.existsSync(plistOldPath)) {
+        fs.renameSync(plistOldPath, plistNewPath);
+        console.log(`Renamed ${plistOldPath} to ${plistNewPath}`);
+    } else {
+        console.log(`File ${plistOldPath} does not exist.`);
+    }
+}
+
+function copyNibFile(srcFile, destDir) {
+    const destFile = path.join(destDir, 'qikfox3D.nib');
+    if (fs.existsSync(srcFile)) {
+        fs.copyFileSync(srcFile, destFile);
+        console.log(`Copied ${srcFile} to ${destFile}`);
+    } else {
+        console.log(`File ${srcFile} does not exist.`);
+    }
+}
+
+function replaceIcnsFiles(dir, srcFile) { 
+    const files = fs.readdirSync(dir, { withFileTypes: true }); 
+    files.forEach(file => {
+        const fullPath = path.join(dir, file.name); 
+        if (file.isFile() && file.name.endsWith('.icns')) { 
+            fs.copyFileSync(srcFile, fullPath);
+        } else if (file.isDirectory() && !file.name.startsWith('.git')) {
+            replaceIcnsFiles(fullPath, srcFile); 
+        }
+    });
+}
+
+function replacePngFile(filePath, srcFile) {
+    if (fs.existsSync(filePath)) {
+        fs.copyFileSync(srcFile, filePath); 
+    } else {
+        console.log(`File ${filePath} does not exist.`);
+    }
+}
+
+function replaceBackgroundPng(dir, srcFile) { 
+    const files = fs.readdirSync(dir, { withFileTypes: true }); 
+    files.forEach(file => { const fullPath = path.join(dir, file.name); 
+        if (file.isFile() && file.name === 'background.png') { 
+            fs.copyFileSync(srcFile, fullPath); 
+        } else if (file.isDirectory() && !file.name.startsWith('.git')) { 
+            replaceBackgroundPng(fullPath, srcFile); 
+        } 
+    });
 }
 
 function main() {
-    const repoUrl = 'https://github.com/waseem-qikfox/qikfox3D-viewer.git';
+    const repoUrl = 'https://github.com/FirestormViewer/phoenix-firestorm.git';
     const cloneDir = 'qikfox3D-viewer';
-    cloneRepository(repoUrl, cloneDir);
+    const commitHash = '8926db526118de57aaa1288bd99f54397aa0d331';
+    const nibFile = path.join(process.cwd(), 'qikfox3D-resources', 'qikfox3D.nib');
+    const iconFile = path.join(process.cwd(), 'qikfox3D-resources', 'qikfox3D_icon.icns');
+    const pngFile = path.join(process.cwd(), 'qikfox3D-resources', 'qikfox3D.png');
+    const targetPngFile = path.join(cloneDir, 'indra', 'newview', 'skins', 'default', 'textures', 'windows', 'login_fs_logo.png');
+    const installerDir = path.join(cloneDir, 'indra', 'newview', 'installers');
+    const backgroundPng = path.join(process.cwd(), 'qikfox3D-resources', 'background.png');
+    
+    var success = cloneRepository(repoUrl, cloneDir, commitHash);
+    if(success === true) {
+        traverseAndReplace(cloneDir, 'Firestorm', 'qikfox3D');
+        renamePlistFile(cloneDir);
+        copyNibFile(nibFile, path.join(cloneDir, 'indra', 'newview'));
+        replaceIcnsFiles(cloneDir, iconFile);
+        replacePngFile(targetPngFile, pngFile);
+        replaceBackgroundPng(installerDir, backgroundPng);
+    }
 }
 
 main();
